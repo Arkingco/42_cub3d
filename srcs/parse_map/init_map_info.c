1eafd1e8879e2110702e91d0acb3435c714e3c87
“brixxt27” <brixxt27@gmail.com>
2022-10-22 22:14:09 +0900

    feat: 중복 없이 식별자 읽었는지 확인#11
    element를 가공하기 전에 모두 NULL 또는 -1로 초기화해서 값을 변경시켰는지
    확인함

--- srcs/parse_map/init_map_info.c
+++ srcs/parse_map/init_map_info.c
@@ -6,7 +6,7 @@
 /*   By: jayoon <jayoon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 13:04:54 by jayoon            #+#    #+#             */
-/*   Updated: 2022/10/22 20:33:43 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/10/22 22:14:04 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -227,6 +227,26 @@ static void	init_element(t_map_info *map_info, int fd)
 		if (cnt_element > 6)
 			break ;
 	}
+	// 중복 없이 모든 식별자를 읽었는지 확인한다.
+	// 그렇다면 map_info의 path가 NULL이 아니다.
+	// ceiling과 floor의 모든 값이 -1이 아니다.
+}
+
+static void	init_map_info_to_initial_value(t_map_info *map_info)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		map_info->texture_path[i] = NULL;
+		if (i < 3)
+		{
+			map_info->ceiling[i] = -1;
+			map_info->floor[i] = -1;
+		}
+		++i;
+	}
 }
 
 void	init_map_info(t_map_info *map_info, int argc, char *file_path)
@@ -236,6 +256,7 @@ void	init_map_info(t_map_info *map_info, int argc, char *file_path)
 	if (check_argc(argc))
 		print_error_str("Argument must be one\n");
 	fd = safe_open(file_path);
+	init_map_info_to_initial_value(map_info);
 	init_element(map_info, fd);
 	// init_map_content(map_info, fd);
 	close(fd);
