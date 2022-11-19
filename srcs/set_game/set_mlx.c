ca7936c69dfd42c0491cfc372f23dfd690ec2780
arkingco <arkingco@gmail.com>
2022-11-19 15:06:37 +0900

    fix : 벽과의 거리 조절 #26

--- srcs/set_game/set_mlx.c
+++ srcs/set_game/set_mlx.c
@@ -6,7 +6,7 @@
 /*   By: kipark <kipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 18:02:00 by kipark            #+#    #+#             */
-/*   Updated: 2022/11/18 21:36:57 by kipark           ###   ########seoul.kr  */
+/*   Updated: 2022/11/19 14:39:59 by kipark           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,27 @@ void	*my_mlx_xpm_file_to_image(void *xvar, char *file, int *width, \
 
 	img = mlx_xpm_file_to_image(xvar, file, width, height);
 	if (img == NULL)
-		print_error_str(MSG_ERR_ELEMENT);
+		print_error_str(MSG_ERR_MLX);
+	return (img);
+}
+
+void	*my_mlx_init(void)
+{
+	void	*mlx;
+
+	mlx = mlx_init();
+	if (mlx == NULL)
+		print_error_str(MSG_ERR_MLX);
+	return (mlx);
+}
+
+
+void	*my_mlx_new_image(void *mlx_ptr, int width, int height)
+{
+	void	*img;
+
+	img = mlx_new_image(mlx_ptr, width, height);
+	if (img == NULL)
+		print_error_str(MSG_ERR_MLX);
 	return (img);
 }
